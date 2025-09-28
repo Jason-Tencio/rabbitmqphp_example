5e558c667da1d74a49fd178956b25cde93c9ab39
Jason-Tencio <jason.tencio18@gmail.com>
2025-09-27 20:06:39 -0400

    Update session_check.php

--- session_check.php
+++ session_check.php
@@ -1,7 +1,8 @@
 <?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
+require_once __DIR__ . '/db_connect.php';
+
+if (empty($_SESSION['user_id'])) {
+    flash('ok', 'Please log in to continue.');
+    header('Location: login.php');
+    exit;
 }
-?>
